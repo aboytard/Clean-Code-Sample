be44ba4ca69dfb16cb3363c9fccd8b708fd4385f
aboytard <alban.boytard@atlascopco.com>
2023-08-01 10:02:35 +0200

    add class implementation examples

---
+++ Clean Code Sample/OO_Class/AbstractTestClass.cs
@@ -0,0 +1,61 @@
+﻿namespace Clean_Code_Sample.OO_Class
+{
+    public class AbstractTestClass
+    {
+        public string Url { get; private set; }
+        public string Name { get; private set; }
+
+        private AbstractTestClass()
+        {
+            Console.WriteLine("initiated..");
+            List<Vehicle> vehicles = new List<Vehicle>();
+            vehicles.Add(new Helicopter());
+            vehicles.Add(new Truck());
+
+            foreach (var vehicle in vehicles)
+            {
+                vehicle.Accelerate();
+            }
+        }
+
+        private AbstractTestClass(string name, string url) : this()
+        {
+            this.Url = url;
+            this.Name = name;
+        }
+
+        public static AbstractTestClass Create(string name, string url)
+        {
+            if (string.IsNullOrWhiteSpace(name)) { throw new ArgumentNullException(); }
+            if (string.IsNullOrWhiteSpace(url)) { throw new ArgumentNullException(); }
+
+            return new AbstractTestClass(name, url);
+        }
+
+        public void Connect()
+        {
+            //this.Url;
+        }
+    }
+
+    public abstract class Vehicle
+    {
+        public abstract void Accelerate();
+    }
+
+    public class Truck : Vehicle
+    {
+        public override void Accelerate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Helicopter : Vehicle
+    {
+        public override void Accelerate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Clean Code Sample/OO_Class/ProtectedTestClass.cs
@@ -0,0 +1,10 @@
+﻿namespace Clean_Code_Sample.OO_Class
+{
+    // use of protected function to for example: Test the method in some unit tests
+    class Point
+    {
+        protected int x;
+        protected int y;
+    }
+    // can be accessed but cannot be changed
+}
