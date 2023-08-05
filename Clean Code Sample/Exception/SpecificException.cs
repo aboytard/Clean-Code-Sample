636462d44e653042752a0bd7aeb329b6db4213d6
aboytard <alban.boytard@atlascopco.com>
2023-08-05 18:36:03 +0200

    add example for specific exception

---
+++ Clean Code Sample/Exception/SpecificException.cs
@@ -0,0 +1,8 @@
+﻿namespace Clean_Code_Sample.Exception
+{
+    public class SpecificException : Exception
+    {
+        public SpecificException(string message) : base(message)
+        { }
+    }
+}
