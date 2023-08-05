9f3c97d4950a1754bfaae14dd45f12d2d51aa034
aboytard <alban.boytard@atlascopco.com>
2023-08-05 20:03:25 +0200

    add special case pattern example

---
+++ Clean Code Sample/Special Case Pattern/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace Clean_Code_Sample.Special_Case_Pattern
+{
+    public class Order
+    {
+        public void PlaceOrder()
+        {
+            Console.WriteLine("Order placed ...");
+        }
+    }
+    
+    // in this special case scenario, we inherit and do nothing from there
+    public class NullOrder : Order
+    {
+        public void PlaceOrder()
+        {
+            Console.WriteLine("Do nothing ...");
+        }
+    }
+}
